99407b8e7a74b167c8904959f5f49cd08aa9e7d8
wushuhong <wushuhong@atomgradient.com>
2025-02-25 20:29:57 +0800

    add:pyproject.toml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="atai-web-tool",
-    version="0.0.1",
+    version="0.0.2",
     description="Extract the main content from a webpage using zendriver, readability-lxml, and BeautifulSoup.",
     author="AtomGradient",
     author_email="alex@atomgradient.com",
